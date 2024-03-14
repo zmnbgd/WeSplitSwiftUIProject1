781ffc56f619a5d641d9186f4a59d178deb3854f
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2024-03-14 12:14:56 +0100

    Add files via upload

---
+++ SwiftUIForm.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftUIForm.swift
+//  WeSplit
+//
+//  Created by Marko Zivanovic on 14.3.24..
+//
+
+import SwiftUI
+
+struct SwiftUIForm: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello world")
+                Text("Hello world")
+            }
+            Section {
+                Text("Hello world")
+                Text("Hello world")
+                Text("Hello world")
+            }
+            Section {
+                Text("Hello world")
+                Text("Hello world")
+                Text("Hello world")
+            }
+            Section {
+                Text("Hello world")
+                Text("Hello world")
+            }
+        }
+    }
+}
+
+#Preview {
+    SwiftUIForm()
+}
