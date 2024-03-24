7cfe352eff85beacc3f363c1b9f44d0f16c121dd
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2024-03-24 23:44:52 +0100

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -14,6 +14,18 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        //MARK:
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -38,7 +50,7 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("We split")
